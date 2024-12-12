0e5dcefd95714d4463c05d982fc4bee6715ed69b
VolkovA <va771@yandex.ru>
2024-12-12 21:12:40 +0300

    Реализовать двусвязный список и методы linkLast, getTasks, removeNode

--- src/tracker/services/InMemoryHistoryManager.java
+++ src/tracker/services/InMemoryHistoryManager.java
@@ -7,8 +7,55 @@ import java.util.List;
 
 //Поставил модификатор доступа по умолчанию.
 //Создавать объекты класса InMemoryTaskManager только в Managers.
-class InMemoryHistoryManager implements HistoryManager {
+class InMemoryHistoryManager<T> implements HistoryManager {
     private final List<Task> history = new ArrayList<>();
+
+    private Node<T> head;   //указатель на первый элемент списка. Он же first
+
+    private Node<T> tail;//указатель на последний элемент списка. Он же last
+
+    /// Добавляет задачу в конец двусвязного списка.
+    private void linkLast(T data) {
+        final Node<T> oldTail = tail;
+        final Node<T> newNode = new Node<>(oldTail, data, null);
+        tail = newNode;
+        if (oldTail == null)
+            head = newNode;
+        else
+            oldTail.next = newNode;
+    }
+
+    /// Выгрузить задачи из двусвязного списка в ArrayList.
+    private ArrayList<T> getTasks() {
+        ArrayList<T> tasks = new ArrayList<>();
+
+        Node<T> iterator = head;
+        while (iterator != null) {
+            tasks.add(iterator.data);
+
+            iterator = iterator.next;
+        }
+
+        return tasks;
+    }
+
+    /// Удалять узел из двусвязного списка.
+    private void removeNode(Node<T> node) {
+        if (node == null) return;
+
+        if (node.prev != null)
+            node.prev.next = node.next;
+        else
+            head = node.next;
+
+        if (node.next != null)
+            node.next.prev = node.prev;
+        else
+            tail = node.prev;
+
+        node.prev = null;
+        node.next = null;
+    }
 
     @Override
     public void add(Task task) {
